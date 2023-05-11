11a7ce09550f4f3b0bd5dc76514d9533db90f10e
YuriTu <tuqiang_ru@163.com>
2023-05-12 01:04:54 +0800

    feat: gpu memory come to cpu

--- main.cpp
+++ main.cpp
@@ -3,6 +3,9 @@
 #include <nvvk/structs_vk.hpp> // 处理各类结构体
 #include <nvvk/resourceallocator_vk.hpp>
 
+static const uint64_t render_width = 800;
+static const uint64_t render_height = 600;
+
 int main(int argc, const char** argv)
 {
     nvvk::ContextCreateInfo deviceInfo;
@@ -26,11 +29,34 @@ int main(int argc, const char** argv)
 
     // memory allocator
     nvvk::ResourceAllocatorDedicated allocator;
+
     // 做了隐式转换，所以type看来其对不上
     allocator.init(context, context.m_physicalDevice);
 
+    // transfer operator destination buffer
+    VkDeviceSize bufferSizeByte = render_width * render_height * 3 * sizeof(float);
+    VkBufferCreateInfo bufferCreateInfo = nvvk::make<VkBufferCreateInfo>();
+    bufferCreateInfo.size = bufferSizeByte;
+    // 用途 storage transfer
+    bufferCreateInfo.usage = VK_BUFFER_USAGE_STORAGE_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT;
+    // 这里不用VkImage的原因单纯是为了简单
+    nvvk::Buffer buffer = allocator.createBuffer(
+        bufferCreateInfo,
+        VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT // 可见性 cpu 可以访问
+        | VK_MEMORY_PROPERTY_HOST_CACHED_BIT // cpu 可caches
+        | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT // cpu gpu 共享性
+    );
 
 
+    // 从GPU传回CPU
+    // todo 1. 所以buffer是开在gpu上的？ map是把storge 映射到cpu raw上？
+    void* data = allocator.map(buffer);
+    float* fltData = reinterpret_cast<float*>(data);
+    printf("First three elements: %f, %f, %f\n", fltData[0], fltData[1], fltData[2]);
+    allocator.unmap(buffer);
+
+    allocator.destroy(buffer);
+    allocator.deinit();
     context.deinit();
 
     
