5e5d1a79fb1a0bfce943f48dfc3043eb2ac5e744
YuriTu <tuqiang_ru@163.com>
2023-05-16 16:37:45 +0800

    wip: update compute shader glsl

--- shaders/raytrace.comp.glsl
+++ shaders/raytrace.comp.glsl
@@ -3,8 +3,31 @@
 
 layout(local_size_x = 16, local_size_y = 8, local_size_z = 1) in;
 
+layout(binding = 0, set = 0, scalar) buffer storageBuffer
+{
+    vec3 imageData[];
+}
+
 void main()
 {
     // debugPrintfEXT("hello world!");
-    debugPrintfEXT("Hello from invocation(%d, %d) !\n", gl_GlobalInvocationID);
+    // debugPrintfEXT("Hello from invocation(%d, %d) !\n", gl_GlobalInvocationID);
+    const uvec2 resolution = uvec2(800, 600);
+
+    const uvec2 pixel = gl_GlobalInvocationID.xy;
+
+    if ((pixel.x >= resolution.x) || (pixel.y >= resolution.y)) 
+    {
+        return;
+    }
+
+    const vec3 pixelColor = vec3(
+        float(pixel.x) / resolution.x,
+        float(pixel.y) / resolution.y,
+        0.0
+    );
+
+    uint linearIndex = resolution.x * pixel.y + pixel.x;
+
+    imageData[linearIndex] = pixelColor;
 }
