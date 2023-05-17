4f7884efa8fa0283413b90e3e835e24c430c3ce3
YuriTu <tuqiang_ru@163.com>
2023-05-17 23:17:21 +0800

    feat: update blas

--- main.cpp
+++ main.cpp
@@ -1,12 +1,16 @@
 
 
+#include <array>
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include <stb_image_write.h>
+#define TINYOBJLOADER_IMPLEMENTATION
+#include <tiny_obj_loader.h>
 
 #include <nvh/fileoperations.hpp>
 #include <nvvk/context_vk.hpp>
 #include <nvvk/descriptorsets_vk.hpp>
 #include <nvvk/error_vk.hpp>
+#include <nvvk/raytraceKHR_vk.hpp>
 #include <nvvk/resourceallocator_vk.hpp>
 #include <nvvk/structs_vk.hpp> // 处理各类结构体
 #include <nvvk/shaders_vk.hpp>
@@ -16,6 +20,38 @@ static const uint64_t render_width = 800;
 static const uint64_t render_height = 600;
 static const uint32_t workgroup_width = 16;
 static const uint32_t workgroup_height = 8;
+
+VkCommandBuffer AllocateAndBeiginOneTimeCommandBuffer(VkDevice device, VkCommandPool cmdPool)
+{
+    VkCommandBufferAllocateInfo cmdAllocInfo = nvvk::make<VkCommandBufferAllocateInfo>();
+    cmdAllocInfo.level                       = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+    cmdAllocInfo.commandPool                 = cmdPool;
+    cmdAllocInfo.commandBufferCount          = 1;
+    VkCommandBuffer cmdBuffer;
+    NVVK_CHECK(vkAllocateCommandBuffers(device, &cmdAllocInfo, &cmdBuffer));
+    VkCommandBufferBeginInfo beginInfo = nvvk::make<VkCommandBufferBeginInfo>();
+    beginInfo.flags                    = VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT;
+    NVVK_CHECK(vkBeginCommandBuffer(cmdBuffer, &beginInfo));
+    return cmdBuffer;
+}
+
+void EndSubmitWaitAndFreeCommandBuffer(VkDevice device, VkQueue queue,VkCommandPool cmdPool, VkCommandBuffer &cmdBuffer)
+{
+    NVVK_CHECK(vkEndCommandBuffer(cmdBuffer));
+    VkSubmitInfo submitInfo       = nvvk::make<VkSubmitInfo>();
+    submitInfo.commandBufferCount = 1;
+    submitInfo.pCommandBuffers    = &cmdBuffer;
+    NVVK_CHECK(vkQueueSubmit(queue, 1, &submitInfo, VK_NULL_HANDLE));
+    NVVK_CHECK(vkQueueWaitIdle(queue));
+    vkFreeCommandBuffers(device, cmdPool, 1, &cmdBuffer);
+}
+
+VkDeviceAddress GetBufferDeviceAddress(VkDevice device, VkBuffer buffer)
+{
+    VkBufferDeviceAddressInfo addressInfo = nvvk::make<VkBufferDeviceAddressInfo>();
+    addressInfo.buffer = buffer;
+    return vkGetBufferDeviceAddress(device, &addressInfo);
+}
 
 int main(int argc, const char** argv)
 {
@@ -75,18 +111,100 @@ int main(int argc, const char** argv)
     );
 
     // 准备shader的情况
-      const std::string        exePath(argv[0], std::string(argv[0]).find_last_of("/\\") + 1);
-        std::vector<std::string> searchPaths = {exePath + PROJECT_RELDIRECTORY, exePath + PROJECT_RELDIRECTORY "..",
-                                          exePath + PROJECT_RELDIRECTORY "../..", exePath + PROJECT_NAME};
+    const std::string        exePath(argv[0], std::string(argv[0]).find_last_of("/\\") + 1);
+    std::vector<std::string> searchPaths = {exePath + PROJECT_RELDIRECTORY, exePath + PROJECT_RELDIRECTORY "..",
+                                        exePath + PROJECT_RELDIRECTORY "../..", exePath + PROJECT_NAME};
+    tinyobj::ObjReader render;
+    render.ParseFromFile(nvh::findFile("scenes/CornellBox-Original-Merged.obj", searchPaths));
+    assert(render.Valid());
+    // 所有的vertex
+    const std::vector<tinyobj::real_t> objVertices = render.GetAttrib().GetVertices();
+    const std::vector<tinyobj::shape_t> objShapes = render.GetShapes();
+    // 一个文件一个mesh
+    assert(objShapes.size() == 1);
+    const tinyobj::shape_t& objShape = objShapes[0];
 
+    // 拿到index 列表 做了一下index_t -> uint32_t
+    std::vector<uint32_t> objIndices;
+    objIndices.reserve(objShape.mesh.indices.size());
+    for(const tinyobj::index_t& index : objShape.mesh.indices)
+    {
+        objIndices.push_back(index.vertex_index);
+    }
 
-
-    // prepare fill memory
+    // prepare data 准备command pool
     VkCommandPoolCreateInfo cmdPoolInfo = nvvk::make<VkCommandPoolCreateInfo>();
     cmdPoolInfo.queueFamilyIndex = context.m_queueGCT;
 
     VkCommandPool cmdPool;
     NVVK_CHECK(vkCreateCommandPool(context, &cmdPoolInfo, nullptr, &cmdPool));
+
+
+    // 准备向gpu写入 vertex 数据
+    nvvk::Buffer vertexBuffer, indexBuffer;
+    {
+        // 准备一个command buffer 专门用于upload vertex
+        VkCommandBuffer uploadCmdBuffer = AllocateAndBeiginOneTimeCommandBuffer(context, cmdPool);
+        // buffer usage 1. 在gpu 上，需要addr 2. storage 可读可写 3. 用于as 的构建
+        const VkBufferUsageFlags usage = VK_BUFFER_USAGE_SHADER_DEVICE_ADDRESS_BIT | VK_BUFFER_USAGE_STORAGE_BUFFER_BIT
+                                     | VK_BUFFER_USAGE_ACCELERATION_STRUCTURE_BUILD_INPUT_READ_ONLY_BIT_KHR;
+        vertexBuffer = allocator.createBuffer(uploadCmdBuffer, objVertices, usage);
+        indexBuffer = allocator.createBuffer(uploadCmdBuffer, objIndices, usage);
+
+        EndSubmitWaitAndFreeCommandBuffer(context, context.m_queueGCT, cmdPool, uploadCmdBuffer);
+        allocator.finalizeAndReleaseStaging();
+    }
+
+    // vertex 和 index 准备好了，则可以准备创建blas
+    // blas有多个，所以是vector, input 类似于blass的constructor 
+    // 1. VkAccelerationStructureGeometryTrianglesDataKHR 说明vertex 和index的信息
+    // 2. VkAccelerationStructureGeometryKHR 第一个图元的情况 
+    // 3. VkAccelerationStructureBuildRangeInfoKHR 数据使用范围
+    std::vector<nvvk::RaytracingBuilderKHR::BlasInput> blases;
+    {
+        nvvk::RaytracingBuilderKHR::BlasInput blas;
+        // 1. VkAccelerationStructureGeometryTrianglesDataKHR 需要获得
+        // vertex index buffer的位置 才能拿到信息做as
+        VkDeviceAddress vertexBufferAddress = GetBufferDeviceAddress(context, vertexBuffer.buffer);
+        VkDeviceAddress indexBufferAddress = GetBufferDeviceAddress(context, indexBuffer.buffer);
+        // 说明具体vertex的drawArrays 的mode 、 数据的格式
+        VkAccelerationStructureGeometryTrianglesDataKHR triangles = nvvk::make<VkAccelerationStructureGeometryTrianglesDataKHR>();
+        triangles.vertexFormat = VK_FORMAT_R32G32B32_SFLOAT;
+        triangles.vertexData.deviceAddress = vertexBufferAddress;
+        // 步长 3个一组
+        triangles.vertexStride                                    = 3 * sizeof(float);
+        // vector.size()
+        triangles.maxVertex                                       = static_cast<uint32_t>(objVertices.size() / 3 - 1);
+        // index的格式
+        triangles.indexType                                       = VK_INDEX_TYPE_UINT32;
+        // index去哪找
+        triangles.indexData.deviceAddress                         = indexBufferAddress;
+        // 没有transform
+        triangles.transformData.deviceAddress                     = 0; 
+
+
+        // 2. VkAccelerationStructureGeometryKHR 说明图元的情况 0. 提供具体vertex data 的指针 1. 是三角形 2. 不透明 没有facedetect的问题
+        VkAccelerationStructureGeometryKHR geometry = nvvk::make<VkAccelerationStructureGeometryKHR>();
+        geometry.geometry.triangles                 = triangles;
+        geometry.geometryType                       = VK_GEOMETRY_TYPE_TRIANGLES_KHR;
+        geometry.flags                              = VK_GEOMETRY_OPAQUE_BIT_KHR;
+        blas.asGeometry.push_back(geometry);
+
+        // 3. 说明数据范围，类似 gl.vertexAttribPointer 说明数据offset与stride
+        VkAccelerationStructureBuildRangeInfoKHR offsetInfo;
+        offsetInfo.firstVertex     = 0;
+        offsetInfo.primitiveCount  = static_cast<uint32_t>(objIndices.size() / 3);  // Number of triangles
+        offsetInfo.primitiveOffset = 0;
+        offsetInfo.transformOffset = 0;
+        blas.asBuildOffsetInfo.push_back(offsetInfo);
+
+        blases.push_back(blas);
+    }
+
+    // 构建具体的blas
+    nvvk::RaytracingBuilderKHR raytracingBuilder;
+    raytracingBuilder.setup(context, &allocator, context.m_queueGCT);
+    raytracingBuilder.buildBlas(blases, VK_BUILD_ACCELERATION_STRUCTURE_PREFER_FAST_TRACE_BIT_KHR);
 
     // 在descriptor set 中 写入一个descriptor 位于bind0 set 0
     // 提供 descriptor 与shader 的access的能力
@@ -126,17 +244,7 @@ int main(int argc, const char** argv)
     ));
     
 
-    VkCommandBufferAllocateInfo cmdAllocInfo = nvvk::make<VkCommandBufferAllocateInfo>();
-    cmdAllocInfo.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
-    cmdAllocInfo.commandPool = cmdPool;
-    cmdAllocInfo.commandBufferCount = 1;
-    VkCommandBuffer cmdBuffer;
-    NVVK_CHECK(vkAllocateCommandBuffers(context, &cmdAllocInfo, &cmdBuffer));
-    
-    // start exec 
-    VkCommandBufferBeginInfo beiginInfo = nvvk::make<VkCommandBufferBeginInfo>();
-    beiginInfo.flags = VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT;
-    NVVK_CHECK(vkBeginCommandBuffer(cmdBuffer, &beiginInfo));
+    VkCommandBuffer cmdBuffer = AllocateAndBeiginOneTimeCommandBuffer(context, cmdPool);
 
     // 填充buffer 一般用于clear或者初始化
     // const float fillValue = 0.5f;
@@ -178,15 +286,9 @@ int main(int argc, const char** argv)
     // 1. fill buffer
     // 2. pipeline barrier
 
-    NVVK_CHECK(vkEndCommandBuffer(cmdBuffer));
 
-    // 命令处理完了，准备queue submit
-    VkSubmitInfo submitInfo = nvvk::make<VkSubmitInfo>();
-    submitInfo.commandBufferCount = 1;
-    submitInfo.pCommandBuffers = &cmdBuffer;
-    NVVK_CHECK(vkQueueSubmit(context.m_queueGCT, 1, &submitInfo, VK_NULL_HANDLE));
-    // await 
-    NVVK_CHECK(vkQueueWaitIdle(context.m_queueGCT));
+    EndSubmitWaitAndFreeCommandBuffer(context, context.m_queueGCT, cmdPool, cmdBuffer);
+
 
     
     // 从GPU传回CPU
@@ -201,7 +303,6 @@ int main(int argc, const char** argv)
     // vkDestroyPipelineLayout(context, pipelineLayout, nullptr);
     descriptorSetContainer.deinit();
 
-    vkFreeCommandBuffers(context, cmdPool, 1, &cmdBuffer);
     vkDestroyCommandPool(context, cmdPool, nullptr);
 
     allocator.destroy(buffer);
