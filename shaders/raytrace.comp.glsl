e97f168514617f7acfccbe3dd103325c2b595880
YuriTu <tuqiang_ru@163.com>
2023-05-18 11:49:08 +0800

    feat: update ray tracing shadering

--- shaders/raytrace.comp.glsl
+++ shaders/raytrace.comp.glsl
@@ -1,6 +1,7 @@
 #version 460
 // #extension GL_EXT_debug_printf : require
 #extension GL_EXT_scalar_block_layout : require
+#extension GL_EXT_ray_query : require
 
 layout(local_size_x = 16, local_size_y = 8, local_size_z = 1) in;
 
@@ -8,6 +9,8 @@ layout(binding = 0, set = 0, scalar) buffer storageBuffer
 {
     vec3 imageData[];
 };
+
+layout(binding = 1, set = 0) uniform accelerationStructureEXT tlas;
 
 void main()
 {
@@ -22,13 +25,43 @@ void main()
         return;
     }
 
-    const vec3 pixelColor = vec3(
-        float(pixel.x) / resolution.x,
-        float(pixel.y) / resolution.y,
-        0.0
+    const vec3 cameraOrigin = vec3(-0.001, 1.0, 6.0);
+    vec3 rayOrigin = cameraOrigin;
+
+    const vec2 screenUV = vec2(
+        // uy / resolution.x  * aspect = uv / res.y
+        (2.0 * (float(pixel.x) + 0.5) - resolution.x) / resolution.y,
+        -(2.0 * (float(pixel.y) + 0.5) - resolution.y) / resolution.y
     );
 
-    uint linearIndex = resolution.x * pixel.y + pixel.x;
+    // const vec2 screenUV = vec2((2.0 * float(pixel.x) + 1.0 - resolution.x) / resolution.y,    //
+    //                          -(2.0 * float(pixel.y) + 1.0 - resolution.y) / resolution.y);  // Flip the y axis
+  
 
-    imageData[linearIndex] = pixelColor;
+    const float fov = 1.0 / 5.0;
+    // z指向外部 右手系
+    vec3 rayDirection = vec3(fov * screenUV, -1.0);
+
+    rayQueryEXT rayQuery;
+    rayQueryInitializeEXT(rayQuery,
+        tlas,
+        gl_RayFlagsOpaqueEXT,
+        0xFF,// mask 类似LOD的处理
+        rayOrigin,
+        0.0,
+        rayDirection,
+        10000.0
+    );
+    // 获得所有intersect(在透明情况下)之后返回false
+    while(rayQueryProceedEXT(rayQuery))
+    {
+
+    }
+    // 拿到t
+    const float t = rayQueryGetIntersectionTEXT(rayQuery, true);
+    // 制作depth buffer 划分为10级
+    vec3 deepthValue = vec3(t / 10.0);
+
+    uint linearIndex = resolution.x * pixel.y + pixel.x;
+    imageData[linearIndex] = vec3(t / 10);
 }
