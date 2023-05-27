ed6cb1d814c1451e99c6b40b4737a7e85781a6ff
YuriTu <tuqiang_ru@163.com>
2023-05-27 15:19:26 +0800

    simulate diffuse box effect

--- shaders/raytrace.comp.glsl
+++ shaders/raytrace.comp.glsl
@@ -75,7 +75,18 @@ HitInfo getObjectHitInfo(rayQueryEXT rayQuery)
     result.worldNormal = objectNormal;
 
     // 目前默认一个albedo
-    result.color = vec3(0.7f);
+    result.color = vec3(0.8f);
+
+    const float dotX = dot(result.worldNormal, vec3(1.0, 0.0, 0.0));
+    if(dotX > 0.99)
+    {
+        result.color = vec3(0.8, 0.0, 0.0);
+    }
+    else if (dotX < -0.99)
+    {
+        result.color = vec3(0.0, 0.8, 0.0);
+    }
+
     return result;
 }
 
